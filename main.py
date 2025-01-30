ad85f4c46fb69b84a63998f8bfa8a9a2a3f54455
Nikita <github@nikiv.dev>
2025-01-30 20:45:51 +0100

    docker

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ def read_root():
     return {"Hello": "World"}
 
 
+@app.get("/hello/{name}")
+def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
